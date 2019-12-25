3332b6e0a12d96eec2b4ed6c681b2380b76a3010
nikolajgriskin <din.nick@bk.ru>
2019-12-25 22:29:41 +0500

    production environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
+  youtube: {
+    apiKey: 'AIzaSyAtZ8Ba5WaFgUjWU1EEC9Q07RY-5D1PW4I',
+    clientId: '236278942648-2h5omtsbj26gs5kmf0vb4td6b25ml3u3.apps.googleusercontent.com',
+  },
   production: true
 };
